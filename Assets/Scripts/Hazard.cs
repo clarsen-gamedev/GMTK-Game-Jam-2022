052892026b8c9ab197e29820136d41ea0656c48c
Connor Larsen <71350625+clarsen-gamedev@users.noreply.github.com>
2022-07-15 17:36:00 -0400

    Create Hazard.cs

---
+++ Assets/Scripts/Hazard.cs
@@ -0,0 +1,34 @@
+// Name: Hazard.cs
+// Author: Connor Larsen
+// Date: 07/15/2022
+// Description: Script which controls the behaviour of the hazard hitboxes in the level
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hazard : MonoBehaviour
+{
+    #region Private Variables
+    private GameManager gameManager;    // Reference to the GameManager script
+    #endregion
+
+    #region Functions
+    // Start is called before the first frame update
+    private void Start()
+    {
+        gameManager = GameObject.FindGameObjectWithTag("Manager").GetComponent<GameManager>();
+    }
+
+    // Check to see if the player has entered the hazard hitbox
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // If the player has collided with the hazard
+        if (collision.tag == "Player")
+        {
+            collision.GetComponent<PlayerController>().enabled = false; // Disable player controls
+            gameManager.GameOver();                                     // End the current run
+        }
+    }
+    #endregion
+}
