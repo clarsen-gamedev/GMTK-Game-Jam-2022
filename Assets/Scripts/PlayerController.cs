5f93bae9f01d79d62c2a94fe619daa1873b220c0
Connor Larsen <71350625+clarsen-gamedev@users.noreply.github.com>
2022-07-15 14:34:35 -0400

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,8 +12,9 @@ public class PlayerController : MonoBehaviour
     #region Public & Serialized Variables
     [SerializeField] float moveSpeed = 1f;  // How fast the player moved
     [SerializeField] float jumpForce = 1f;  // How high the player jumps
+    [SerializeField] float floatForce = 1f; // How much force is applied with the flap action
 
-    public enum ControlScheme {NONE, BASIC, FLAP, GRAVITY}; // Enum types for all possible control schemes the player has
+    public enum ControlScheme {BASIC, FLAP, GRAVITY, NONE}; // Enum types for all possible control schemes the player has
     public ControlScheme controlScheme;                     // Reference to the currently used control scheme
     #endregion
 
@@ -42,14 +43,47 @@ public class PlayerController : MonoBehaviour
         {
             if (controlScheme == ControlScheme.BASIC)   // If the control scheme is set to BASIC...
             {
-                rigidbody.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse); // Add an upwards force to the player's Rigidbody
+                if (Mathf.Abs(rigidbody.velocity.y) < 0.001f)   // Check to see if the player is on solid ground
+                {
+                    rigidbody.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse); // Add an upwards force to the player's Rigidbody
+                }
+            }
+
+            else if (controlScheme == ControlScheme.FLAP)   // If the control scheme is set to FLAP...
+            {
+                rigidbody.velocity = new Vector2(0f, 0f);                               // Reset velocity
+                rigidbody.AddForce(new Vector2(0, floatForce), ForceMode2D.Impulse);    // Add an upwards force to the player's Rigidbody
+            }
+
+            else if (controlScheme == ControlScheme.GRAVITY)    // If the control scheme is set to GRAVITY...
+            {
+                if (rigidbody.gravityScale == 1)
+                {
+                    rigidbody.gravityScale = -1;
+                }
+                else
+                {
+                    rigidbody.gravityScale = 1;
+                }
             }
         }
 
         // DEBUG Switch State
-        if (Input.GetKeyDown(KeyCode.Q))
+        if (Input.GetKeyDown(KeyCode.Alpha1))   // Switch to BASIC
         {
-            controlScheme = controlScheme++;
+            controlScheme = ControlScheme.BASIC;
+            Debug.Log("Set control scheme to " + controlScheme);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))   // Switch to FLAP
+        {
+            controlScheme = ControlScheme.FLAP;
+            Debug.Log("Set control scheme to " + controlScheme);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))   // Switch to GRAVITY
+        {
+            controlScheme = ControlScheme.GRAVITY;
             Debug.Log("Set control scheme to " + controlScheme);
         }
     }
