c678e066d548ffa9bafa9987a4d564e882a3eb78
Connor Larsen <71350625+clarsen-gamedev@users.noreply.github.com>
2022-07-16 01:41:11 -0400

    Update RouletteValueCheck.cs

--- Assets/Scripts/RouletteValueCheck.cs
+++ Assets/Scripts/RouletteValueCheck.cs
@@ -1,3 +1,8 @@
+// Name: RouletteValueCheck.cs
+// Author: Connor Larsen
+// Date: 07/15/2022
+// Description: Control the colliders on the roulette wheel
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,21 +10,43 @@ using UnityEngine;
 public class RouletteValueCheck : MonoBehaviour
 {
     #region Public Variables
-    public int requiredFace;    // Which face of the die is needed to pass the check
-    #endregion
+    [Header("Variables")]
+    public int requiredFace;        // Which face of the die is needed to pass the check
+    public bool isActive;           // If the collider is active or not
+    public Animator gateAnimator;   // Animator attached to the gate which gets lowered
 
-    #region Private Variables
+    [Header("Materials")]
+    [SerializeField] Material inactive;
+    [SerializeField] Material active;
     #endregion
 
     #region Functions
+    private void Awake()
+    {
+        isActive = false;
+    }
+
+    private void Update()
+    {
+        if (isActive)
+        {
+            gameObject.GetComponent<MeshRenderer>().material = active;
+        }
+        else
+        {
+            gameObject.GetComponent<MeshRenderer>().material = inactive;
+        }
+    }
+
     private void OnTriggerEnter(Collider collider)
     {
-        if (collider.tag == "Player")   // If the player enters the hitbox...
+        if (collider.tag == "Player" && isActive)   // If the player enters the hitbox of an active collider...
         {
             // If the face matches the required value...
             if (collider.gameObject.GetComponent<CheckDieValue>().currentSide == requiredFace)
             {
-                Destroy(gameObject);    // Destroy this collider
+                gateAnimator.SetTrigger("GateOpen");
+                gameObject.SetActive(false);
             }
         }
     }
